e2fbc1a5e072b2a94f282fc75ab2ec1c910c81b6
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-23 13:38:14 +0200

    fix: fixed formattng and handler logic

--- src/main/java/com/example/javashopspring/Exceptions/handler/GlobalExceptionHandler.java
+++ src/main/java/com/example/javashopspring/Exceptions/handler/GlobalExceptionHandler.java
@@ -7,11 +7,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -19,23 +20,27 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(JavaShopException.class)
     public ResponseEntity<ErrorMessageDto> handleException(JavaShopException exception) {
         return ResponseEntity.status(exception.getStatus())
-                .body(new ErrorMessageDto(exception.getMessage(), exception.getStatus().value(), LocalDate.now()));
+                .body(new ErrorMessageDto(exception.getMessage(), exception.getStatus().value(), LocalDateTime.now()));
     }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorMessageDto> handleUnexpected(Exception e) {
         logger.error("Unexpected error", e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(new ErrorMessageDto("Unexpected error", 500, LocalDate.now()));
+                .body(new ErrorMessageDto("Unexpected error", 500, LocalDateTime.now()));
     }
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorMessageDto> handleMethodArgument(MethodArgumentNotValidException e){
+        StringBuilder errorFields = new StringBuilder();
+        for (FieldError error : e.getBindingResult().getFieldErrors()){
+            errorFields.append(error.getField()).append(": ").append(error.getDefaultMessage());
+        }
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorMessageDto(e.getMessage(),e.getStatusCode().value(),LocalDate.now()));
+                .body(new ErrorMessageDto(errorFields.toString(),e.getStatusCode().value(),LocalDateTime.now()));
     }
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ErrorMessageDto> handleMessageNotReadable(HttpMessageNotReadableException e){
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorMessageDto(e.getMessage(),400,LocalDate.now()));
+                .body(new ErrorMessageDto(e.getMessage(),400,LocalDateTime.now()));
     }
 }
