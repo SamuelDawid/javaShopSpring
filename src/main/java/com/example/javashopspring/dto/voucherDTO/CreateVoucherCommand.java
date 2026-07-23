afba40cf7c1e8565e57e6f9fef45c9e96fa9d1cc
Samuel Dawid <141030100+SamuelDawid@users.noreply.github.com>
2026-07-23 13:36:28 +0200

    feat: added valdiadtion to dto

--- src/main/java/com/example/javashopspring/dto/voucherDTO/CreateVoucherCommand.java
+++ src/main/java/com/example/javashopspring/dto/voucherDTO/CreateVoucherCommand.java
@@ -1,8 +1,17 @@
 package com.example.javashopspring.dto.voucherDTO;
 
+import jakarta.validation.constraints.*;
+
 import java.time.LocalDate;
 
-public record CreateVoucherCommand(String voucherName,
-                                   LocalDate expirationDate,
-                                   int percentage) {
+public record CreateVoucherCommand(
+        @NotBlank
+        String voucherName,
+        @NotNull
+        @Future(message = "expiration date must be ahead ")
+        LocalDate expirationDate,
+        @Positive(message = "percentage must be positive")
+        @Max(25)
+        int percentage) {
 }
+
